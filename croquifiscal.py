dd37c0063a2331ba269293eae0a739ed0255543e
mbaliu <38162096+mbaliu@users.noreply.github.com>
2019-02-05 14:23:32 -0200

    Update croquifiscal.py

--- croquifiscal.py
+++ croquifiscal.py
@@ -9,9 +9,9 @@ uri_prodam = 'http://sf9402.app.prodam/intranet/frmConsultaCroquiPDF.aspx?pstrSe
 uri_pub = 'http://geosampa.prefeitura.sp.gov.br/PaginasPublicas/DownloadCroqui.aspx?setor={0:0>3}&quadra={1:0>3}.format(setor, quadra)
 fileobj = urllib.request.urlopen (uri_pub)
 
+# print(url)
+
 # Cria um novo arquivo e salva o conteúdo
-file = open(r'path\to\folder\{0:0>3}_{1:0>3}.pdf'.format(setor, quadra), 'bw')
+file = open(r'path\to\folder\CADASTRO Croqui_Fiscal\CF {0:0>3}_{1:0>3}.pdf'.format(setor, quadra), 'bw')
 file.write(fileobj.read())
 file.close()
-
-print(url)
