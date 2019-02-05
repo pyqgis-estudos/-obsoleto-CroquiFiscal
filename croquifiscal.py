04ce8954953f487fd662edb7ce2dd11c86bb42fc
mbaliu <38162096+mbaliu@users.noreply.github.com>
2019-02-05 14:31:57 -0200

    Update croquifiscal.py

--- croquifiscal.py
+++ croquifiscal.py
@@ -4,14 +4,28 @@ import urllib
 setor = 299
 quadra = 47
 
-# Requerimento ao endereço da prodam
-uri_prodam = 'http://sf9402.app.prodam/intranet/frmConsultaCroquiPDF.aspx?pstrSetor={0:0>3}&pstrQuadra={1:0>3}'.format(setor, quadra)
-uri_pub = 'http://geosampa.prefeitura.sp.gov.br/PaginasPublicas/DownloadCroqui.aspx?setor={0:0>3}&quadra={1:0>3}.format(setor, quadra)
-fileobj = urllib.request.urlopen (uri_pub)
+# SQ dado
+SQ_Lista = [[setor, quadra],]
+# Lista de SQ
+SQ_Lista = [[,],[,],[,]]
+# SQ das feições selecionadas
+SQ_Lista = []
+for feat in features:
+  setor = 
+  quadra = 
+  SQ_Lista.append([setor, quadra])
+  
 
-# print(url)
+# Loop para cada SQ
+for SQ in SQ_Lista:
+  # Requerimento ao endereço da prodam
+  uri_prodam = 'http://sf9402.app.prodam/intranet/frmConsultaCroquiPDF.aspx?pstrSetor={0:0>3}&pstrQuadra={1:0>3}'.format(SQ[0], SQ[1])
+  uri_pub = 'http://geosampa.prefeitura.sp.gov.br/PaginasPublicas/DownloadCroqui.aspx?setor={0:0>3}&quadra={1:0>3}'.format(SQ[0], SQ[1])
+  fileobj = urllib.request.urlopen (uri_pub)
 
-# Cria um novo arquivo e salva o conteúdo
-file = open(r'path\to\folder\CADASTRO Croqui_Fiscal\CF {0:0>3}_{1:0>3}.pdf'.format(setor, quadra), 'bw')
-file.write(fileobj.read())
-file.close()
+  # print(url)  # Verificação da uri
+
+  # Cria um novo arquivo e salva o conteúdo
+  file = open(r'path\to\folder\CADASTRO Croqui_Fiscal\CF {0:0>3}_{1:0>3}.pdf'.format(SQ[0], SQ[1]), 'bw')
+  file.write(fileobj.read())
+  file.close()
